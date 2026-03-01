0ea7a62453b786042410f217f2d7413f80b28ddc
bexruzbek174 <braxmonov205@gmail.com>
2026-03-01 21:06:02 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import os
 import telebot
 import sqlite3
+from flask import Flask
+import threading
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 CHANNEL_ID = -1003747471816
 
+# DATABASE
 conn = sqlite3.connect("movies.db", check_same_thread=False)
 cursor = conn.cursor()
 
@@ -18,7 +21,17 @@ CREATE TABLE IF NOT EXISTS movies (
 """)
 conn.commit()
 
+# Flask app
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Bot ishlayapti 🚀"
+
+def run_bot():
+    bot.infinity_polling()
+
+# Kanalga yangi kino tushsa
 @bot.channel_post_handler(content_types=['video', 'document'])
 def save_movie(message):
     cursor.execute("SELECT COUNT(*) FROM movies")
@@ -34,7 +47,7 @@ def save_movie(message):
         reply_to_message_id=message.message_id
     )
 
-
+# User kod yuborsa
 @bot.message_handler(func=lambda message: True)
 def send_movie(message):
     code = message.text
@@ -43,14 +56,12 @@ def send_movie(message):
     result = cursor.fetchone()
 
     if result:
-        bot.copy_message(
-            message.chat.id,
-            CHANNEL_ID,
-            result[0]
-        )
+        bot.copy_message(message.chat.id, CHANNEL_ID, result[0])
     else:
         bot.send_message(message.chat.id, "❌ Bunday kino topilmadi")
 
+# Botni alohida threadda ishga tushiramiz
+threading.Thread(target=run_bot).start()
 
-bot.infinity_polling()
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
